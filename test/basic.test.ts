f5ce6766f650f2944a86b0e19e5c1e2c9e9beff5
Eckhardt-D <eckhardt.dreyer@gmail.com>
2023-04-26 06:10:39 +0200

    fix: tests

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,7 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { fileURLToPath } from "node:url";
 import { setup, $fetch } from "@nuxt/test-utils";
-import fs from "fs";
 
 describe("ssr", async () => {
   await setup({
@@ -10,7 +9,6 @@ describe("ssr", async () => {
 
   it("renders the video element with attrs passed through", async () => {
     const response = await $fetch("/");
-    fs.writeFileSync("./test.html", response);
     expect(response).toMatch(`<video width="960" controls>`);
   });
 });
